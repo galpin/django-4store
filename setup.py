a2f47107ffe47b394c807207155ca8e54f9587f0
Martin Galpin <m@66laps.com>
2011-01-23 22:34:14 +0000

    Bump version number to "0.2.1".

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(fname):
 
 setup(
     name = "django-4store",
-    version = "0.2",
+    version = "0.2.1",
     url = "http://github.com/66laps/django-fourstore",
     license = "LGPL v3",
     description = "A small Django application that makes developing with the 4Store RDF database easier.",
